eb64e600925e750a6ebb518fa4ac6f12883f2b1d
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2019-08-24 21:39:31 +0530

    Check for long mode

--- TestA20.asm
+++ TestA20.asm
@@ -21,31 +21,39 @@ TestA20:
 ;else it's fine
 ;we'll choose our magic number, stored in 0x7dfe
 
-pusha
+pusha	;push all registers
 
-mov ax,[0x7dfe]
-mov dx,ax
-call printh
+mov ax,[0x7dfe]	;move [0x7dfe] which is aa55
+mov cx,ax	;copy to cx register
+
+;call printh	;debugging
 
 ;0xffff0+offset = 0x107dfe
 ;offset = 0x107dfe-0xffff0 = 0x7e0e
 ;if A20 is disabled then we get the location as 7dfe
 ;but if enabled we don't
 
-call SegTest
-mov ax,0x2400
-int 15h
-call SegTest
-popa
+;call SegTest 	;debugging
+;mov ax,0x2400	;debugging. part of enabling the a20 line
+;int 15h	;debugging. part of enabling the a20 line
+
+call SegTest	;actual testing here
+
+;call EnableA20 
 ret
+
 SegTest:
-push bx
-xor bx,bx
-sub bx,1
-mov es,bx
-pop bx
-mov bx,0x7e0e ;basically sending the offset to check
-mov dx,[es:bx];if aa55 then there's a wrap, otherwise no
-call printh
+push bx		;save previous value
+xor bx,bx	;zero the bx register
+sub bx,1	;bx = 0xffff
+mov es,bx	;es = 0xffff
+pop bx		;get the previous saved value
+mov bx,0x7e0e	;basically sending the offset to check
+		;es:bx = 0xffff0+0x7e0e = 0x107dfe
+		;if a20 is disabled we get 0x7dfe, which has the value 0xaa55
+mov dx,[es:bx]	;if aa55 then there's a wrap, otherwise no
+sub dx,cx	;dx - cx . cx has [0x7dfe] which is aa55
+		;zero flag will be set if zero
+popa
 ret
 ;Turns out QEMU does enable the A20 line by default
--- bootsect.asm
+++ bootsect.asm
@@ -4,10 +4,13 @@ section .data ; constants, put under the magic number, at the end
 section .bss  ; variables, similarly at the end
 section .text ; code
 global main   ;main is global and is our entry point
+
 main:
 ;clear the seg registers, to prevent any offset buffer
+
 cli ;clear interrupts 
 jmp 0x0000:ZeroSeg
+
 ZeroSeg:
 	xor ax,ax  	; clear ax
 	mov ss,ax
@@ -36,43 +39,59 @@ call readDisk
 
 call sect2 		; works if sect2 is read
 
-call TestA20		;Testing if a20 line is enabled
+mov ax,0x2400		;Disable A20
+int 15h
+
+;call TestA20		;Testing if a20 line is enabled
+call EnableA20
 
 ;mov dx,[0x7dfe] 	; 0x7c00+512
 ;call printh		; testing the printh subroutine
-jmp $
+;jmp $
 
 %include "./printf.asm"
 %include "./readDisk.asm"
 %include "./printh.asm"
 %include "./TestA20.asm"
-
+%include "./EnableA20.asm"
 MESSAGE db "Hello, World!",0x0a,0x0d,0
 DISKSUCCESSMSG db "Welcome to my first OS!",0ah,0dh,0
+
+LM_SUCCESS db "Long Mode is available",10,13,0
+ID_FAIL db "CPUID UNSUPPORTED",10,0
+EXFUNC_FAIL db "Extended functions UNSUPPORTED",10,0
+LM_FAIL db "Long Mode unavailable",10,0
 
 times 510 - ($-$$) db 0
 dw 0xaa55
 
 
-;sector 2 code
+;sector 2  
 sect2:
-	mov ax,0013h
-	int 0x10
-	push 0a000h
-	pop es
-	mov cx,0
-	scrfill:
-		mov [es:di],dword 3
-		inc di
-		loop scrfill
-	keyb:
-		mov ah,00h
-		int 16h
-		cmp ah,0
-		je keyb
-	mov ax,0x0003
-	int 0x10
+	mov si, MESSAGE
+	call printf
+	call checklm
+	
+;	mov ax,0013h
+;	int 0x10
+;	push 0a000h
+;	pop es
+;	mov cx,0
+;	scrfill:
+;		mov [es:di],dword 3
+;		inc di
+;		loop scrfill
+;	keyb:
+;		mov ah,00h
+;		int 16h
+;		cmp ah,0
+;		je keyb
+;	mov ax,0x0003
+;	int 0x10*/
+
 	mov si,DISKSUCCESSMSG
 	call printf
-	ret
+	jmp $
+
+%include "./checklm.asm"
 times 512-($-sect2) db 0
